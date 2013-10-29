77ef685ba835bf33177f81f466f9009b1ae63a95
thuyltm <thuybk04>
2013-10-29 15:55:24 +0700

    Commit messages

---
+++ AdDB/src/main/java/com/mkyong/common/mongodb/ConnectDBUtil.java
@@ -0,0 +1,81 @@
+package com.mkyong.common.mongodb;
+
+import com.mongodb.*;
+
+import java.net.UnknownHostException;
+import java.util.Date;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: thuyltm
+ * Date: 10/29/13
+ * Time: 3:37 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ConnectDBUtil {
+    public static void connectDB(){
+        try {
+
+            /**** Connect to MongoDB ****/
+            // Since 2.10.0, uses MongoClient
+            MongoClient mongo = new MongoClient("localhost", 27017);
+
+            /**** Get database ****/
+            // if database doesn't exists, MongoDB will create it for you
+            DB db = mongo.getDB("testdb");
+
+            /**** Get collection / table from 'testdb' ****/
+            // if collection doesn't exists, MongoDB will create it for you
+            DBCollection table = db.getCollection("user");
+
+            /**** Insert ****/
+            // create a document to store key and value
+            BasicDBObject document = new BasicDBObject();
+            document.put("name", "mkyong");
+            document.put("age", 30);
+            document.put("createdDate", new Date());
+            table.insert(document);
+
+            /**** Find and display ****/
+            BasicDBObject searchQuery = new BasicDBObject();
+            searchQuery.put("name", "mkyong");
+
+            DBCursor cursor = table.find(searchQuery);
+
+            while (cursor.hasNext()) {
+                System.out.println(cursor.next());
+            }
+
+            /**** Update ****/
+            // search document where name="mkyong" and update it with new values
+            BasicDBObject query = new BasicDBObject();
+            query.put("name", "mkyong");
+
+            BasicDBObject newDocument = new BasicDBObject();
+            newDocument.put("name", "mkyong-updated");
+
+            BasicDBObject updateObj = new BasicDBObject();
+            updateObj.put("$set", newDocument);
+
+            table.update(query, updateObj);
+
+            /**** Find and display ****/
+            BasicDBObject searchQuery2
+                    = new BasicDBObject().append("name", "mkyong-updated");
+
+            DBCursor cursor2 = table.find(searchQuery2);
+
+            while (cursor2.hasNext()) {
+                System.out.println(cursor2.next());
+            }
+
+            /**** Done ****/
+            System.out.println("Done");
+
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (MongoException e) {
+            e.printStackTrace();
+        }
+    }
+}
